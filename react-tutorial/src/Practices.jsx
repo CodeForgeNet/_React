3647b4f8a581cd14edbde0743d7c172e6bb317ed
CodeForgeNet <codeforgenet@icloud.com>
2025-04-16 09:34:58 +0530

    Update files

--- react-tutorial/src/Practices.jsx
+++ react-tutorial/src/Practices.jsx
@@ -1,8 +1,11 @@
 export const Practices = () => {
-  const students = [];
+  const students = [1];
   return (
     <>
-      <p> {students.length && "No students found"} </p>
+      {/* <p> {students.length && "No students found"} </p>  */}{" "}
+      {/* this is wrong */}
+      {/* 1st */}
+      <p> {students.length === 0 && "No students found"} </p>
       <p>Number of students : {students.length} </p>
     </>
   );
--- react-tutorial/src/source.md
+++ react-tutorial/src/source.md
@@ -3,3 +3,4 @@ React does not render `false`, `null`,`undefined` or `NAN` in the DOM. These val
 However `0` and empty Strings (`""`) are exceptions :
 
 - **`0`** is rendered in the DOM ,coz it considered a valid React node.
+- **Empty strings** (`""`) are also cosidered valid and renderd as well.
