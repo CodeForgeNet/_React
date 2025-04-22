f42dab422d2b5284681dcc8f9a680145602246b7
CodeForgeNet <codeforgenet@icloud.com>
2025-04-22 14:39:20 +0530

    Initial commit

--- react-tutorial/src/Practices.jsx
+++ react-tutorial/src/Practices.jsx
@@ -1,11 +1,13 @@
 export const Practices = () => {
-  const students = [1];
+  const students = [2, 3];
   return (
     <>
       {/* <p> {students.length && "No students found"} </p>  */}{" "}
       {/* this is wrong */}
       {/* 1st */}
-      <p> {students.length === 0 && "No students found"} </p>
+      {/* <p> {students.length === 0 && "No students found"} </p> */}
+      {/* 2nd */}
+      <p> {!students.length && "No students found"} </p>
       <p>Number of students : {students.length} </p>
     </>
   );
